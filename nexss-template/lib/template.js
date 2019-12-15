a0fb2dbcf746531a2fba346368e7c6da37d2a481
Marcin Polak <mapoart@gmail.com>
2019-12-15 10:20:34 +0100

    Removed whole path on the template selection and removed config files (.js) from the list so only templates are displayed.

--- nexss-template/lib/template.js
+++ nexss-template/lib/template.js
@@ -1,9 +1,9 @@
 const nexssLanguages = require("../../nexss-language/lib/language");
 const { getFiles } = require("../../lib/fs");
-const { join, extname, dirname, resolve } = require("path");
+const { join, extname, dirname } = require("path");
 module.exports.templateNames = arg => {
   if (arg) {
-    // We remove chalk characters
+    // We remove colors characters if exist
     arg = arg.split(" ")[0];
     arg = arg.replace("\u001b[1m", "");
     arg = arg.replace("\u001b[22m", "");
@@ -13,13 +13,23 @@ module.exports.templateNames = arg => {
   // if (arg) files = files.filter(e => path.extname(e) === arg);
   //return [arg];
 
-  return files.map(
-    element =>
-      `${extname(element)} ${element} - (${resolve(
-        dirname(lang.configFile),
-        "templates",
-        element
-      )})`
+  // return files.map(
+  //   element =>
+  //     `${extname(element)} ${element} - (${resolve(
+  //       dirname(lang.configFile),
+  //       "templates",
+  //       element
+  //     )})`
+  // );
+
+  return (
+    files
+      // We need to filter as there are also configuration files .js to
+      // make additional operation
+      .filter(el => {
+        return extname(el) === arg;
+      })
+      .map(element => `${extname(element)} ${element}`)
   );
 };
 
