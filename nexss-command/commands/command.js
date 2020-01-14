f1b1fad0745784cfae51de514f0233b0eebc3397
Marcin Polak <mapoart@gmail.com>
2020-01-14 15:29:01 +0100

    Added documentation to Nexss P Commands + Cleanup

--- nexss-command/commands/command.js
+++ nexss-command/commands/command.js
@@ -1,18 +1,21 @@
 const { NEXSS_PROJECT_CONFIG_PATH } = require("../../config/config");
 const { loadConfigContent } = require("../../lib/config");
 let configContent = loadConfigContent(NEXSS_PROJECT_CONFIG_PATH);
+const { bold } = require("../../lib/color");
 
 const { spawn } = require("child_process");
 const { listCommands } = require("../lib/commands");
 if (!configContent) {
-  console.log("You are not in the nexss programmer project folder.");
+  console.log(`You are not in the ${bold("Nexss Programmer")} project folder.`);
   process.exit(1);
 }
 
+// Setup commands names as default is run the command.
 if (["add", "delete", "list"].includes(process.argv[3])) {
   require(`./${process.argv[3]}.js`);
 }
 
+// Find command in the config by the name
 const CommandToRun = configContent.findByProp(
   "commands",
   "name",
@@ -20,11 +23,13 @@ const CommandToRun = configContent.findByProp(
 );
 
 if (CommandToRun) {
+  // We get the first command for spawn
   const commandArray = CommandToRun.command.split(" ");
   let commandToRun = commandArray[0];
   commandArray.shift();
 
-  const proc = spawn(commandToRun, commandArray, {
+  // Execute command and display in the stdio
+  spawn(commandToRun, commandArray, {
     stdio: "inherit",
     shell: true
   });
--- nexss-command/lib/commands.js
+++ nexss-command/lib/commands.js
@@ -1,25 +1,34 @@
 const { NEXSS_PROJECT_CONFIG_PATH } = require("../../config/config");
 const { loadConfigContent } = require("../../lib/config");
 let configContent = loadConfigContent(NEXSS_PROJECT_CONFIG_PATH);
-const { red, yellow, bold, blue } = require("../../lib/color");
+const { red, yellow, bold, blue, green } = require("../../lib/color");
 
 function listCommands() {
   const commands = configContent.commands;
 
   if (!commands || (commands && commands.length == 0)) {
     console.log(
-      red(`No available commands. (use eg. nexss c add *name* ls -l)`)
+      red(bold(`No available commands. (use eg. nexss c add *name* ls -l)`))
     );
     process.exit(0);
   }
   console.log(
     blue(
-      `Available predefined commands in _nexss.yml: (usage: nexss command *name*)`
+      bold(
+        `Available predefined commands in _nexss.yml: (usage: nexss command *name*)`
+      )
     )
   );
-  commands.map(e =>
-    console.log(`${bold(e.name)} - ${yellow(bold(e.command))}`)
-  );
+
+  const Table = require("cli-table3");
+  var table = new Table({
+    head: [green("name"), green("command")]
+  });
+  commands.forEach(cmd => {
+    table.push([bold(yellow(cmd.name)), bold(cmd.command)]);
+  });
+
+  console.log(table.toString());
 }
 
 module.exports.listCommands = listCommands;
