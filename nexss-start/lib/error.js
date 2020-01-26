801dcc290054d8ec72352d4c366fa4aaae39ec62
Marcin Polak <mapoart@gmail.com>
2020-01-26 11:47:30 +0100

    Added named groups for error solutions

--- nexss-start/lib/error.js
+++ nexss-start/lib/error.js
@@ -2,6 +2,7 @@ const { error } = require("../../lib/log");
 const { bold, yellow } = require("../../lib/color");
 const { getLangByFilename } = require("../../nexss-language/lib/language");
 
+// more here: https://github.com/nexssp/cli/wiki/Errors-Solutions
 module.exports.parseError = (filename, errorBody, stdOutput) => {
   const filenameStore = filename;
   const langInfo = getLangByFilename(filename);
@@ -27,11 +28,12 @@ module.exports.parseError = (filename, errorBody, stdOutput) => {
   );
   // console.log("errrrrrrrrorrrrrssss", langInfo.errors);
   // process.exit(1);
+  let solutionNumber = 1;
   if (langInfo.errors) {
     Object.keys(langInfo.errors).forEach(pattern => {
-      let regExp = new RegExp(pattern, "i");
-      let match = regExp.exec(errorBody);
-      let solution;
+      let regExp = new RegExp(pattern, "gi");
+      let match = errorBody.matchAll(regExp);
+      var solution;
 
       if (
         !Array.isArray(langInfo.errors[pattern]) &&
@@ -44,26 +46,42 @@ module.exports.parseError = (filename, errorBody, stdOutput) => {
           solution = langInfo.errors[pattern]["all"];
         }
       }
-      if (match && match.length > 1) {
+
+      //Capturing naming groups
+      // https://javascript.info/regexp-groups#named-groups
+      const ArrayMatch = Array.from(match);
+      if (ArrayMatch && ArrayMatch.length > 0) {
+        if (ArrayMatch[0].groups) {
+          Object.keys(ArrayMatch[0].groups).forEach(e => {
+            solution = solution.replace(`<${e}>`, ArrayMatch[0].groups[e]);
+          });
+        }
+      } else if (match && match.length > 1) {
         //console.log("find: ", pattern);
         //console.log(match);
         // We display errors to standard output (eg --server)
-        const psolution = `Possible solution:\n ${bold(yellow(solution))
+        solution = solution
           .replace(/<package>/g, match[1])
           .replace(/<module>/g, match[1])
-          .replace(/<found1>/g, match[1])}`;
-        if (stdOutput) {
-          console.log(psolution);
-        } else {
-          console.error(psolution);
-        }
+          .replace(/<found1>/g, match[1]);
       } else if (errorBody.includes(pattern)) {
         // We display errors to standard output (eg --server)
+
+        return;
+      } else {
+        solution = null;
+      }
+      if (solution) {
         if (stdOutput) {
-          console.log(`Possible solution: ${solution}`);
+          console.log(`Possible solution ${solutionNumber}: ${solution}`);
         } else {
-          console.error(yellow(`Possible solution: ${bold(yellow(solution))}`));
+          console.error(
+            yellow(
+              `Possible solution ${solutionNumber}: ${bold(yellow(solution))}`
+            )
+          );
         }
+        solutionNumber++;
       }
     });
   }
