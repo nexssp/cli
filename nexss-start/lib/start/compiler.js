8abfbb6e00af64926260a91738c88e914d7f875a
Marcin Polak <mapoart@gmail.com>
2020-06-04 12:34:13 +0200

    added compiler definition at the top

--- nexss-start/lib/start/compiler.js
+++ nexss-start/lib/start/compiler.js
@@ -3,11 +3,13 @@ const path = require("path");
 // We get compiler
 // if it is in the globalConfig ./nexss/config.json
 //
-const { info } = require("../../../lib/log");
+const fs = require("fs");
+const { info, warn } = require("../../../lib/log");
 const cliArgs = require("minimist")(process.argv.slice(2));
 
 module.exports.getCompiler = (file) => {
   const fileName = file.name;
+  const fileFillPAth = path.join(file.path, file.name);
   const languageDefinition = getLangByFilename(fileName);
 
   const extension = path.extname(fileName).slice(1);
@@ -17,43 +19,71 @@ module.exports.getCompiler = (file) => {
 
   // We check for custom compiler at the top of the file
 
-  // GLOBAL COMPILER
-  let globalLangConfig;
-  const globalConfigPath = require("os").homedir() + "/.nexss/config.json";
-  if (require("fs").existsSync(globalConfigPath)) {
-    globalConfig = require(globalConfigPath);
-  } else {
-    globalConfig = { languages: {} };
-  }
+  if (path.extname(fileName) && fs.existsSync(fileName)) {
+    if (cliArgs.verbose) {
+      info(`Checking for the config in the file.. (top of the file)`);
+    }
 
-  if (
-    globalConfig &&
-    globalConfig.languages &&
-    globalConfig.languages[path.extname(fileName).slice(1)]
-  ) {
-    globalLangConfig = globalConfig.languages[path.extname(fileName).slice(1)];
-
-    if (ld_compiler[globalLangConfig.compilers]) {
-      compiler = languageDefinition.compilers[globalLangConfig.compilers];
-      // console.log(compiler);
-      if (cliArgs.verbose) {
-        info(
-          `Compiler has been set to ${compiler} from global config file ${globalConfigPath}`
-        );
-      }
+    // TODO: Later fix for efficient, read only lines which are needed
+    // The one which starts with nexss-
+    const fileContent = fs.readFileSync(fileName).toString().split(/\r?\n/);
+    const maxConfigLines = 20; // fileContent.length
+    for (let i = 0; i < maxConfigLines; i++) {
+      const line = fileContent[i];
+      if (!line.includes("nexss-") || !line.includes(":")) break;
+      const splitter = line.split("nexss-")[1].split(":");
+      file[splitter[0]] = splitter[1].trim();
     }
   }
 
-  if (process.nexssGlobalConfig.languages[extension]) {
-    globalLangConfig = process.nexssGlobalConfig.languages[extension];
+  if (file.compiler && !ld_compiler[file.compiler]) {
+    if (cliArgs.verbose) {
+      warn(
+        `Compiler has been set to ${file.compiler} from file but not exists. Using default one.`
+      );
+    }
+    delete file.compiler;
+  }
+  if (!file.compiler) {
+    // GLOBAL COMPILER
+    let globalLangConfig;
+    const globalConfigPath = require("os").homedir() + "/.nexss/config.json";
+    if (require("fs").existsSync(globalConfigPath)) {
+      globalConfig = require(globalConfigPath);
+    } else {
+      globalConfig = { languages: {} };
+    }
 
-    if (ld_compiler[globalLangConfig.compilers]) {
-      compiler = languageDefinition.compilers[globalLangConfig.compilers];
-      // console.log(compiler);
-      if (cliArgs.verbose) {
-        info(
-          `Compiler has been set to ${compiler} from global config file ${globalConfigPath}`
-        );
+    if (
+      globalConfig &&
+      globalConfig.languages &&
+      globalConfig.languages[path.extname(fileName).slice(1)]
+    ) {
+      globalLangConfig =
+        globalConfig.languages[path.extname(fileName).slice(1)];
+
+      if (ld_compiler[globalLangConfig.compilers]) {
+        compiler = languageDefinition.compilers[globalLangConfig.compilers];
+        // console.log(compiler);
+        if (cliArgs.verbose) {
+          info(
+            `Compiler has been set to ${compiler} from global config file ${globalConfigPath}`
+          );
+        }
+      }
+    }
+
+    if (process.nexssGlobalConfig.languages[extension]) {
+      globalLangConfig = process.nexssGlobalConfig.languages[extension];
+
+      if (ld_compiler[globalLangConfig.compilers]) {
+        compiler = languageDefinition.compilers[globalLangConfig.compilers];
+        // console.log(compiler);
+        if (cliArgs.verbose) {
+          info(
+            `Compiler has been set to ${compiler} from global config file ${globalConfigPath}`
+          );
+        }
       }
     }
   }
@@ -65,7 +95,7 @@ module.exports.getCompiler = (file) => {
       compiler = ld_compiler[fileCompilerSplit[0]];
 
       fileCompilerSplit.shift();
-      compiler.args = fileCompilerSplit.concat(compilerArgs).join(" ");
+      compiler.args = fileCompilerSplit.concat(compiler.args).join(" ");
     }
   } else {
     if (!compiler) {
