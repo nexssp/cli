2c41f11541f437079fb0cf2069a172b63ca2d4e9
Marcin Polak <mapoart@gmail.com>
2020-07-21 09:15:36 +0200

    Added better wsl (Windows Subsystem Linux) handling (Crystal, Ada.. languages)

---
+++ config/wslInstallInfo.js
@@ -0,0 +1,14 @@
+const { bold } = require("../lib/color");
+module.exports = () => {
+  console.log(`${bold("wsl")} command has not been found.`);
+  console.log(
+    `For this language you need to have ${bold(
+      "Windows Subsystem Linux (WSL)"
+    )} enabled.`
+  );
+  console.log(
+    `More information you can find here: ${bold(
+      "https://github.com/nexssp/cli/wiki/WSL-(Windows-Subsystem-Linux)"
+    )}`
+  );
+};
