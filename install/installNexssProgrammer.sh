14968042443df6eb892c7934d448eeedbdd59b1d
Marcin Polak <mapoart@gmail.com>
2020-09-01 16:32:08 +0200

    Fix issues with the installer

--- install/installNexssProgrammer.sh
+++ install/installNexssProgrammer.sh
@@ -1,6 +1,6 @@
 # Nexss Programmer Linux Installer
 InstallPath=~/.nexss/nexssCli
-if test -f "$FILE"; then
+if ! test -f "$InstallPath"; then
     git clone --depth=1 https://github.com/nexssp/cli.git $InstallPath
     echo Adding $InstallPath to your PATH
     if ! grep "~/.nexss/nexssCli" ~/.bashrc &> /dev/null ; then
@@ -17,12 +17,13 @@ if test -f "$FILE"; then
     else
         echo Node is installed
         node -v
+        echo NPM
         npm -v
     fi   
     # First start + install packages
     nexss
 else
-    echo $InstallPath already exists (Nexss Programmer is installed)
+    echo $InstallPath already exists - Nexss Programmer is installed
     echo to update it there see 'nexss --update'
     echo or you can delete $InstallPath and start installer again
 fi
