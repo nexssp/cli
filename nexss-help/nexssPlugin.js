ae5d982a498cb1c494029b407c697c4cc161ff4f
Marcin Polak <mapoart@gmail.com>
2020-09-02 14:37:48 +0200

    Version adjustment

--- nexss-help/nexssPlugin.js
+++ nexss-help/nexssPlugin.js
@@ -2,5 +2,5 @@ module.exports = {
   version: "1.0",
   author: "Marcin Polak <mapoart@gmail.com>",
   installPath: "./",
-  description: "Module for displaying help"
+  description: "Module for displaying help",
 };
