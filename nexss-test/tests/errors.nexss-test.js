8c9a9d1ada52bd2bda612f72278905c5a9faedd9
Marcin Polak <mapoart@gmail.com>
2020-09-10 11:13:59 +0200

    typo fix

--- nexss-test/tests/errors.nexss-test.js
+++ nexss-test/tests/errors.nexss-test.js
@@ -61,7 +61,7 @@ module.exports = {
           title: "Check String Solution",
           params: [
             `nexss Nexss/Test/ErrorsSolutions/src/errors3.js`,
-            "Possible solution 1: Did you forget semi-color ';' ?",
+            "Possible solution 1: Did you forget semi-colon ';' ?",
           ],
         },
       ],
