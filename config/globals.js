9f86482a79b8c3168483cdb5c7aeb3812d10bfb8
Marcin Polak <mapoart@gmail.com>
2020-09-16 08:47:03 +0200

    Added path and dev_colors to globals

--- config/globals.js
+++ config/globals.js
@@ -1,6 +1,11 @@
 Object.assign(global, require("../lib/ansi"));
+
+const dev_colors = require("../lib/core/dev-colors");
+
 const functions = {
   fs: require("fs"),
+  path: require("path"),
+  dev_colors,
 };
 
 Object.assign(global, functions);
