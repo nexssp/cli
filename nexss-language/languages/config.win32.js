fde55fbf80c1bdfd0520aa39b437f672427f0832
Marcin Polak <mapoart@gmail.com>
2021-04-21 15:24:09 +0200

    Some syntax cleanup

--- nexss-language/languages/config.win32.js
+++ nexss-language/languages/config.win32.js
@@ -1,16 +1,16 @@
-let c = require('./config.base');
+let c = require("./config.base");
 c.osPackageManagers = {
   scoop: {
     installation:
-      'Set-ExecutionPolicy RemoteSigned -scope CurrentUser ; powershell -command "iex (new-object net.webclient).downloadstring(\'https://get.scoop.sh\')"',
-    installCommand: 'scoop install',
+      "Set-ExecutionPolicy RemoteSigned -scope CurrentUser ; powershell -command \"iex (new-object net.webclient).downloadstring('https://get.scoop.sh')\"",
+    installCommand: "scoop install",
   },
   choco: {
     installation:
       '@"%SystemRoot%System32WindowsPowerShell\v1.0powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString(\'https://chocolatey.org/install.ps1\'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%chocolatey\bin"',
-    installCommand: 'choco install',
+    installCommand: "choco install",
   },
 };
-c.compilerInstallation = 'scoop install';
+c.compilerInstallation = "scoop install";
 
 module.exports = c;
