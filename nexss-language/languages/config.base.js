d15e15576bba372f0b28ce5d6b43d77bf546d136
Marcin Polak <mapoart@gmail.com>
2021-04-25 10:34:14 +0200

    Changed from .executeCommandLine to .run + some cleanups.

--- nexss-language/languages/config.base.js
+++ nexss-language/languages/config.base.js
@@ -1,16 +1,14 @@
 module.exports = {
-    url: "https://nexss.com",
-    author: "mapoart@gmail.com",
-    title: "", // example: NodeJS
-    description: null, // displays on the language lists, example: Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine.
-    url: null, // example: "https://nodejs.org"
-    executeCommandLine: null, // example: "node -e"
-    printCommandLine: null, // example: node -p // no print needed for display content
-    interactiveShell: null, // example: node // starts interactive shell
-    checkSyntax: null, // example node -c // check systax of the program (no run)
-    builders: {}, // example: https://github.com/nexssp/language_nodejs/blob/master/nodejs.win32.nexss.config.js
-    compilers: {}, // example: https://github.com/nexssp/language_nodejs/blob/master/nodejs.win32.nexss.config.js
-    osPackageManagers: null,
-    languagePackageManagers: null,
-    errors: null // example: require("./nexss.nodejs.errors");
+  title: "", // example: NodeJS
+  description: null, // displays on the language lists, example: Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine.
+  url: null, // example: "https://nodejs.org"
+  run: null, // example: "node -e"
+  printCommandLine: null, // example: node -p // no print needed for display content
+  interactiveShell: null, // example: node // starts interactive shell
+  checkSyntax: null, // example node -c // check systax of the program (no run)
+  builders: {}, // example: https://github.com/nexssp/language_nodejs/blob/master/nodejs.win32.nexss.config.js
+  compilers: {}, // example: https://github.com/nexssp/language_nodejs/blob/master/nodejs.win32.nexss.config.js
+  osPackageManagers: null,
+  languagePackageManagers: null,
+  errors: null, // example: require("./nexss.nodejs.errors");
 };
